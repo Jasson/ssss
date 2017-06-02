7184d0f1886e16204ac2277708273b86f7505bf5
langxw <langxw1@lenovo.com>
2017-06-02 17:48:06 +0800

    first commit

---
+++ reverse_test.go
@@ -0,0 +1,23 @@
+package stringutil
+
+import ("testing"
+        "fmt"
+        )
+
+func TestReverse(t *testing.T) {
+    cases := []struct {
+        in, want string
+    }{
+        {"Hello, world", "dlrow ,olleH"},
+        {"Hello, 世界", "界世 ,olleH"},
+        {"", ""},
+    }
+    for _, c := range cases {
+        fmt.Println("sssss")
+        got := Reverse(c.in)
+        if got != c.want {
+            t.Errorf("Reverse(%q) == %q, want %q", c.in, got, c.want)
+        }
+    }
+}
+
